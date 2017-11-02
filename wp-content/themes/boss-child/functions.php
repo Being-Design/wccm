1522702fb2054ecf19b335ead2c1c785a7662316
Mike Trischetta <mr.trischetta@gmail.com>
2017-11-02 12:36:43 -0400

    Added option to hide mobile menu icons

--- wp-content/themes/boss-child/functions.php
+++ wp-content/themes/boss-child/functions.php
@@ -128,6 +128,11 @@ function bd_theme_settings_page() {
         <td><input type="checkbox" name="display_item_nav_in_profile" <?php if ( get_option('display_item_nav_in_profile') ) echo 'checked'; ?> value="true" /></td>
       </tr>
 
+      <tr valign="top">
+        <th scope="row">Hide icons to left of mobile menu items</th>
+        <td><input type="checkbox" name="hide_mobile_menu_icons" <?php if ( get_option('hide_mobile_menu_icons') ) echo 'checked'; ?> value="true" /></td>
+      </tr>
+
     </table>
 
     <?php submit_button(); ?>
@@ -146,6 +151,7 @@ function register_bdsettings() {
   register_setting( 'bd-options-group', 'hide_profile_stats' );
   register_setting( 'bd-options-group', 'display_groups_in_profile' );
   register_setting( 'bd-options-group', 'display_item_nav_in_profile' );
+  register_setting( 'bd-options-group', 'hide_mobile_menu_icons' );
   
 }
 
