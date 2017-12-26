2bd15c59528cc323139a15ac0eeb227f8bb7860d
Mike Trischetta <mr.trischetta@gmail.com>
2017-12-26 15:24:59 -0500

    Setting default buddypress icons

--- wp-content/themes/boss-child/functions.php
+++ wp-content/themes/boss-child/functions.php
@@ -271,3 +271,7 @@ function custom_override_checkout_fields( $fields ) {
   unset($fields['billing']['billing_phone']);
   return $fields;
 }
+
+// Set default BuddyPress avatars
+define ( 'BP_AVATAR_DEFAULT', get_stylesheet_directory() . '/images/avatar-member.jpg' );
+define ( 'BP_AVATAR_DEFAULT_THUMB', get_stylesheet_directory() . '/images/avatar-member.jpg' );
